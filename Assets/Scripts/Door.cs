aeb40736d7f76f172d7f8472173ad2c2b0d72e09
XAKER131 <malyga.2018@gmail.com>
2021-02-27 18:59:44 +0300

    project int commit
     i pushed all files of my unity game

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+	private bool inZone = false;
+   	private void OnTriggerEnter2D( Collider2D collision )	{
+		inZone = true;
+		Debug.Log("Вы у двери");
+	}
+	private void OnTriggerExit2D( Collider2D collision)	{
+		inZone = false;
+		Debug.Log("Вы ушли от двери");
+	}
+
+	public int scene;
+	void Update() {  
+	if (Input.GetKeyDown("space") && inZone)
+        {
+            print("space key was pressed");
+            SceneManager.LoadScene(scene);
+            }
+    }
+}
---
+++ Assets/Scripts/ya_itishnik_chestno.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ya_itishnik_chestno: MonoBehaviour 
+{
+    public void PressedStartLvl( int sceneID )    
+{
+        SceneManager.LoadScene(sceneID);
+    	}
+
+    public void PressedExit()    
+{
+        Application.Quit();
+    	}
+
+}
