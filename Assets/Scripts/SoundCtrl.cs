30292e09a41217a8b28c3ccbc45c5fc89f479576
XAKER131 <malyga.2018@gmail.com>
2021-04-24 18:24:20 +0300

    +meta

---
+++ Assets/Scripts/SoundCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(PolygonCollider2D))]
+public class SoundCtrl : MonoBehaviour {
+    private AudioSource frazeClip;
+    public bool repitable = false;
+    private bool isPlayed = false;
+
+    private void Awake()    {
+        frazeClip = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)    {
+        if (!isPlayed || repitable)        {
+            frazeClip.Play();
+            isPlayed = true;
+        }
+    }
+}
